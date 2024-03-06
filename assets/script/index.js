6036a6c11188e3848c716042be03809daf04487d
MARCO DELPOPOLO <156794727+MarcoDelpimme@users.noreply.github.com>
2024-03-06 15:14:51 +0100

    commit2
    commit2

--- assets/script/index.js
+++ assets/script/index.js
@@ -3,5 +3,6 @@ document.getElementById("searchForm").addEventListener("submit", function (event
 
   const artistName = document.getElementById("artistName").value;
 
+  //encodeURIComponent lho usato per far si che se nel form vengano inseriti degli spazi o dei caratteri speciali essi vengano convertiti senza causa problemi durante il reindirazzamento
   window.location.href = `artistTestpage.html?artist=${encodeURIComponent(artistName)}`;
 });
